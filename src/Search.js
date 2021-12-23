4eb11fe452a23f93f8163349e57fe75e96bbfe96
sarahc987 <89328572+sarahc987@users.noreply.github.com>
2021-12-23 17:14:58 +1000

    Updated Search.js

--- src/Search.js
+++ src/Search.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./search.css";
 
 export default function Search() {
+  const [city, setCity] = useState("Brisbane");
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(`${city}`);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
   return (
     <div className="Search">
-      <form id="search-form">
+      <form id="search-form" onSubmit={handleSubmit}>
         <input
           type="search"
           placeholder="Enter a city"
           id="city-search"
           autocomplete="off"
+          onChange={updateCity}
         />
         <input type="submit" value="search" className="searchButton" />
       </form>
